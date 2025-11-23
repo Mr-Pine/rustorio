667aa7fe732447c95e9746e87729821e9bb8850e
Albert Garde <albertsgarde@gmail.com>
2025-11-23 20:40:13 +0100

    :memo: Update docs

--- docs/src-files.js
+++
@@ -1,2 +0,0 @@
-createSrcSidebar('[["rustorio",["",[],["buildings.rs","gamemodes.rs","lib.rs","recipes.rs","resources.rs","tick.rs"]]]]');
-//{"start":19,"fragment_lengths":[99]}
--- docs/trait.impl/core/clone/trait.Clone.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["rustorio",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"rustorio/enum.ResourceType.html\" title=\"enum rustorio::ResourceType\">ResourceType</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[276]}
